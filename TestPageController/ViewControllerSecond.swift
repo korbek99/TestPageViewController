4ebd4a6f150a1fb90a50ffd1005540e54a274e91
korbek100 <korbek100@gmail.com>
2018-04-03 20:36:52 -0300

    first commit

---
+++ TestPageController/ViewControllerSecond.swift
@@ -0,0 +1,25 @@
+//
+//  ViewControllerSecond.swift
+//  TestPageController
+//
+//  Created by Jose David Bustos H on 03-04-18.
+//  Copyright © 2018 Jose David Bustos H. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ViewControllerSecond: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
